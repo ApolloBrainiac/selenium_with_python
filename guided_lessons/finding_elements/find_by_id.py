113c1dcd6a21c1209bd772c76f9b27758a3e98cc
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-05 15:18:33 -0500

    Begin finding elements lessons

---
+++ guided_lessons/finding_elements/find_by_id.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+
+class FindByIdName():
+
+	def test(self):
+		driver = webdriver.Firefox()
+		driver.get("https://letskodeit.teachable.com/p/practice")
+		elementById = driver.find_element_by_id('name')
+
+		if elementById is not None:
+			print("We found an elment by ID")
+
+		elementByName = driver.find_element_by_name('show-hide')
+
+		if elementByName is not None:
+			print("We found an element by Name")
+
+ff = FindByIdName()
+ff.test()
