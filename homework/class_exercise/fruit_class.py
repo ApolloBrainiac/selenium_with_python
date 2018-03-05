2a17be62b4b905898ebcbe24416332a5e5709abf
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-05 03:22:47 -0500

    Add Fruit class exercise instructions

---
+++ homework/class_exercise/fruit_class.py
@@ -0,0 +1,23 @@
+"""
+Create a class Fruit
+Define 3 methods in this class
+	__init__()
+	nutrition()
+	fruit_shape()
+
+Print anything you like in these methods
+
+Create one more class (Any fruit name)
+This class should inhereit from the Fruit class created above, this
+will become the child class and "Fruit" will be referred as parent
+class to this class
+Define 3 methods in thise class
+	__init__()
+	nutrition()
+	color()
+Print anything you like in these methods
+
+Created instances of these classes and call methods from them
+Call methods from the base class also using the instance of the
+child class
+"""
