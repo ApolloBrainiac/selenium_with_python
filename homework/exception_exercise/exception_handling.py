3f107d566656c1a56069b22312fa910e4ec0cc49
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-05 04:43:48 -0500

    Create pick_color function and add car dictionary

--- homework/exception_exercise/exception_handling.py
+++ homework/exception_exercise/exception_handling.py
@@ -1,7 +1,7 @@
 """
 Exception Handling Exercise
 
-Create a dictionar "car"
+Create a dictionary "car"
 Create 3 keys
 	- make
 	- model
@@ -11,3 +11,15 @@ Try to access the color key. Remember, we never created the color key,
 so it's going to throw an exception. You need to handle the exception
 using try, except and finally block
 """
+# Color access function
+def pick_color():
+	try:
+		print('Oh my ex-girlfriend\'s car is ' + car['color'] + '.')
+	except:
+		print('You know, I don\'t really remember what color her car is.')
+	finally:
+		print('Actually, all my ex\'s drive Jettas. Weird now that I think about it.')
+
+
+# Car dictionary
+car = {'make': 'VW', 'model': 'Jetta', 'year': '2005'}
