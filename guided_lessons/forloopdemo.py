c5705302442f802d217fe9513dee9ed3f9b986cc
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-04 23:21:13 -0500

    Finish guided for loop lesson

--- guided_lessons/forloopdemo.py
+++ guided_lessons/forloopdemo.py
@@ -11,3 +11,23 @@ for c in my_string:
 		print('A', end=' ')
 	else:
 		print(c, end=' ')
+
+cars = ['bmw', 'benz', 'honda']
+
+for car in cars:
+	print(car)
+
+nums = [1, 2, 3]
+
+for n in nums:
+	print(n * 10, end= ' ')
+
+d = {'one': 1, 'two': 2, 'three': 3}
+for k in d:
+	print(k + " " + str(d[k]), end=" ")
+
+print("*"*20)
+
+for k,v in d.items():
+	print(k)
+	print(v)
