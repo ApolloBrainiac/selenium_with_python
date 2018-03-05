96df9971342f9e0be1879fd60a223acb0419c768
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-05 04:46:13 -0500

    Finalize exercise with a clean test and PEP8 formatting

--- homework/exception_exercise/exception_handling.py
+++ homework/exception_exercise/exception_handling.py
@@ -11,15 +11,19 @@ Try to access the color key. Remember, we never created the color key,
 so it's going to throw an exception. You need to handle the exception
 using try, except and finally block
 """
-# Color access function
+
+
 def pick_color():
-	try:
-		print('Oh my ex-girlfriend\'s car is ' + car['color'] + '.')
-	except:
-		print('You know, I don\'t really remember what color her car is.')
-	finally:
-		print('Actually, all my ex\'s drive Jettas. Weird now that I think about it.')
+    # Color access function
+    try:
+        print('Oh my ex-girlfriend\'s car is ' + car['color'] + '.')
+    except:
+        print('You know, I don\'t really remember what color her car is.')
+    finally:
+        print('Actually, all my ex\'s drive Jettas. Weird now that I think about it.')
 
 
 # Car dictionary
 car = {'make': 'VW', 'model': 'Jetta', 'year': '2005'}
+
+pick_color()
