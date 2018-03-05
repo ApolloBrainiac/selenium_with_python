58a94ff2ff1eaa391972ba05c3265458ab074af9
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-05 04:43:28 -0500

    Include exception exercise instrucitons

---
+++ homework/exception_exercise/exception_handling.py
@@ -0,0 +1,13 @@
+"""
+Exception Handling Exercise
+
+Create a dictionar "car"
+Create 3 keys
+	- make
+	- model
+	- year
+
+Try to access the color key. Remember, we never created the color key,
+so it's going to throw an exception. You need to handle the exception
+using try, except and finally block
+"""
