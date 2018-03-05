3097836be4eab421a53a2946ff2f43371b623458
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-05 01:27:12 -0500

    Include assignment instructions

---
+++ homework/tax_method.py
@@ -0,0 +1,12 @@
+"""
+Create a method, which takes the state and gross income
+as arguments and returns the net income after deducting
+tax based on the state.
+
+Assume Federal Tax: 10%
+Assume state tax on your wish.
+
+You don't have to do for all the states, just take 3-4 to solve
+the purpose of the exercise
+"""
+
