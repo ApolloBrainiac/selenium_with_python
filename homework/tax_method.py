6a4261a01c9640f955ef17e9b76b111be2572037
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-05 01:39:34 -0500

    Set variables for state tax rates and salaries

--- homework/tax_method.py
+++ homework/tax_method.py
@@ -11,8 +11,32 @@ the purpose of the exercise
 """
 def net_income(state_tax, gross_income):
 	fed_tax = .1
+	#federal tax rate is a constant
+
 	fed_sum = gross_income * fed_tax
 	state_sum = gross_income * state_tax
+	"""
+	these variables determine how much
+	is owed to state and federal institutions
+	"""
+
 	net = gross_income - (fed_sum + state_sum)
+	#shows how much tax is removed from gross
 	return net
 
+# tax rates to be used for different states
+
+co_tax = .0463
+#Colorado
+
+il_tax = .0375
+#Illinois
+
+ma_tax = .0510
+#Massachusetts
+
+pa_tax = .0307
+#Pennsylvania
+
+salary1 = 50000
+salary2 = 100000
