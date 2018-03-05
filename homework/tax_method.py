41c3b5f987cb3ae5c4fca936ff3eec2108bf33a1
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-05 01:31:53 -0500

    First pass at defining 'net_income' function

--- homework/tax_method.py
+++ homework/tax_method.py
@@ -9,4 +9,10 @@ Assume state tax on your wish.
 You don't have to do for all the states, just take 3-4 to solve
 the purpose of the exercise
 """
+def net_income(state_tax, gross_income):
+	fed_tax = .1
+	fed_sum = gross_income * fed_tax
+	state_sum = gross_income * state_tax
+	net = gross_income - (fed_sum + state_sum)
+	return net
 
