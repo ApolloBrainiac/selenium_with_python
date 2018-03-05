cd15155384ae508b14e3497cbcf1465b6ef46453
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-05 15:50:15 -0500

    Begin xpath/css selector lesson

--- guided_lessons/finding_elements/find_by_id.py
+++ guided_lessons/finding_elements/find_by_id.py
@@ -1,19 +1,21 @@
 from selenium import webdriver
 
+
 class FindByIdName():
 
-	def test(self):
-		driver = webdriver.Firefox()
-		driver.get("https://letskodeit.teachable.com/p/practice")
-		elementById = driver.find_element_by_id('name')
+    def test(self):
+        driver = webdriver.Firefox()
+        driver.get("https://letskodeit.teachable.com/p/practice")
+        elementById = driver.find_element_by_id('name')
 
-		if elementById is not None:
-			print("We found an elment by ID")
+        if elementById is not None:
+            print("We found an elment by ID")
 
-		elementByName = driver.find_element_by_name('show-hide')
+        elementByName = driver.find_element_by_name('show-hide')
 
-		if elementByName is not None:
-			print("We found an element by Name")
+        if elementByName is not None:
+            print("We found an element by Name")
+
 
 ff = FindByIdName()
 ff.test()
