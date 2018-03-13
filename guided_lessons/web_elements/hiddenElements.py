9c189a0157f80cc1df485e2af448f4ecc4538479
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-13 17:59:37 -0400

    Set up hidden elements exercise

---
+++ guided_lessons/web_elements/hiddenElements.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+class HiddenElements():
+	def test(self):
+		baseUrl = "https://letskodeit.teachable.com/p/practice"
+		driver = webdriver.Firefox()
+		driver.maximize_window()
+		driver.get(baseUrl)
+		driver.implicitly_wait(10)
+
+
+he = HiddenElements()
+he.test()
