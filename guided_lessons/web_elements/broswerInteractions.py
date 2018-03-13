88f197ffa3a16a873eafcecb059ea19d083df237
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-13 13:52:13 -0400

    Run clean test for browserInteractions

--- guided_lessons/web_elements/broswerInteractions.py
+++ guided_lessons/web_elements/broswerInteractions.py
@@ -1,34 +1,47 @@
 from selenium import webdriver
 
+
 class BroswerInteractions():
 
-	def test(self):
-		baseUrl= 'https://letskodeit.teachable.com/p/practice'
-		driver = webdriver.Firefox()
+    def test(self):
+        baseUrl = 'https://letskodeit.teachable.com/p/practice'
+        driver = webdriver.Firefox()
 
-		# Window Maximize
+        # Window Maximize
+        driver.maximize_window()
 
-		# Open the Url
+        # Open the Url
+        driver.get(baseUrl)
 
-		# Get Title
+        # Get Title
+        title = driver.title
+        print("Title of the web page is: " + title)
 
-		print("Title of the web page is: ")
+        # Get Current Url
+        url = driver.current_url
+        print("Current Url of the web page is: " + url)
 
-		# Get Current Url
+        # Broswer Refresh
+        driver.refresh()
+        print("Browser Refreshed 1st time")
+        driver.get(driver.current_url)
+        print("Browser Refreshed 2nd time")
 
-		print("Current Url of the web page is: ")
+        # Open another Url
+        driver.get(
+            'https://sso.teachable.com/secure/42299/users/sign_in?clean_login=true&reset_purchase_session=1')
+        # Browser Back
+        driver.back()
+        print('Go one step back in browser history')
 
-		# Broswer Refresh
-
-		print("Browser Refreshed 1st time")
-
-		print("Browser Refreshed 2nd time")
-
-		# Open another Url
-
-		# Browser Back		
-
+        # Browser Forward
+        driver.forward()
+        print('Go one step forward in broswer history')
+        # Get Page Source
+        pageSource = driver.page_source
+        # Broswer Close / Quit
+        driver.close()
 
 
 bi = BroswerInteractions()
-bi.test
+bi.test()
