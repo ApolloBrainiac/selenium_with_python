3554bba55a711a7cfc99f47b228ee9f75bedca8a
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-13 15:45:46 -0400

    Include element varaibles and methods

--- guided_lessons/web_elements/radioButtonsAndCheckboxes.py
+++ guided_lessons/web_elements/radioButtonsAndCheckboxes.py
@@ -1,13 +1,30 @@
 from selenium import webdriver
+import time
+
 
 class RadioButtons():
 
-	def test(self):
-		baseUrl = 'https://letskodeit.teachable.com'
-		driver = webdriver.Firefox()
-		driver.maximize_window()
-		driver.get(baseUrl)
-		driver.implicitly_wait(10)
+    def test(self):
+        baseUrl = 'https://letskodeit.teachable.com/p/practice'
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(10)
+
+        bmwRadioBtn = driver.find_element_by_id('bmwradio')
+        bmwRadioBtn.click()
+
+        time.sleep(2)
+        benzRadioBtn = driver.find_element_by_id('benzradio')
+        benzRadioBtn.click()
+
+        time.sleep(2)
+        bmwCheckBox = driver.find_element_by_id('bmwcheck')
+        bmwCheckBox.click()
+
+        time.sleep(2)
+        benzCheckBox = driver.find_element_by_id('benzcheck')
+        benzCheckBox.click()
 
 
 rb = RadioButtons()
