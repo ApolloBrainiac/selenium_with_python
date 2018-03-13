931b74fb3576b1e02a1af70ece4a07a88a24146d
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-13 16:11:46 -0400

    Include test functions and run clean test

--- guided_lessons/web_elements/workingWithElementsList.py
+++ guided_lessons/web_elements/workingWithElementsList.py
@@ -1,11 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
 
 class ElementLists():
-	def test(self):
-		baseUrl = 'https://letskodeit.teachable.com/p/practice'
-		driver = webdriver.Firefox()
-		driver.maximize_window()
-		driver.get(baseUrl)
+    def test(self):
+        baseUrl = 'https://letskodeit.teachable.com/p/practice'
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(10)
+
+        radioButtonsList = driver.find_elements(By.XPATH,
+                                                "//input[contains(@type, 'radio') and contains(@name, 'cars')]")
+        size = len(radioButtonsList)
+        print("Size of the list: " + str(size))
+
+        for radioButton in radioButtonsList:
+            isSelected = radioButton.is_selected()
+
+            if not isSelected:
+                radioButton.click()
+                time.sleep(2)
+
 
 el = ElementLists()
 el.test()
