4508cc71db99f836e45784d7c892e9d800885887
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-13 15:02:18 -0400

    Run clean test for element state

--- guided_lessons/web_elements/elementState.py
+++ guided_lessons/web_elements/elementState.py
@@ -2,9 +2,15 @@ from selenium import webdriver
 
 
 class ElementState():
-    def isEnable(self):
+    def isEnabled(self):
         baseUrl = 'https://www.google.com'
         driver = webdriver.Firefox()
         driver.maximize_window()
         driver.get(baseUrl)
         driver.implicitly_wait(3)
+
+        e1 = driver.find_element_by_id('lst-ib')
+        e1.send_keys('letskodeit')
+
+es = ElementState()
+es.isEnabled()
