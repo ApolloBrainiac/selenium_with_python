dc4f0634a82a210bcb751175da0dc034cbc8ef68
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-13 17:37:22 -0400

    Implement Select method and run clean test

--- guided_lessons/web_elements/dropdownSelect.py
+++ guided_lessons/web_elements/dropdownSelect.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 import time
 
 
@@ -11,18 +12,22 @@ class DropDown():
         driver.get(baseUrl)
         driver.implicitly_wait(10)
 
+        el1 = driver.find_element(By.ID, 'carselect')
+        sel = Select(el1)
+
+        sel.select_by_value("benz")
         print("Select Benz by value")
-
         time.sleep(2)
 
+        sel.select_by_index("2")
         print("Select Honda by index")
-
         time.sleep(2)
 
+        sel.select_by_visible_text("BMW")
         print("Select BMW by visible text")
-
         time.sleep(2)
 
+        sel.select_by_index(2)
         print("Select Honda by index")
 
 dd = DropDown()
