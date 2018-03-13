7bb0ff49d444df4c0527973c4d3169249c6555da
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-13 17:06:12 -0400

    Include print statments

--- guided_lessons/web_elements/dropdownSelect.py
+++ guided_lessons/web_elements/dropdownSelect.py
@@ -2,13 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-class DropDown():
-	def test(self):
-		baseUrl = 'https://letskodeit.teachable.com/p/practice'
-		driver = webdriver.Firefox()
-		driver.maximize_window()
-		driver.implicitly_wait(10)
 
+class DropDown():
+    def test(self):
+        baseUrl = 'https://letskodeit.teachable.com/p/practice'
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(10)
+
+        print("Select Benz by value")
+
+        time.sleep(2)
+
+        print("Select Honda by index")
+
+        time.sleep(2)
+
+        print("Select BMW by visible text")
+
+        time.sleep(2)
+
+        print("Select Honda by index")
 
 dd = DropDown()
 dd.test()
