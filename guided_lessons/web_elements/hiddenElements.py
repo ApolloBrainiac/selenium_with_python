e48d8fccc6dd11fe8046af7d9870a4fbd0d28afe
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-14 17:55:38 -0400

    Include function for Expedia test

--- guided_lessons/web_elements/hiddenElements.py
+++ guided_lessons/web_elements/hiddenElements.py
@@ -4,13 +4,39 @@ import time
 
 
 class HiddenElements():
-	def test(self):
-		baseUrl = "https://letskodeit.teachable.com/p/practice"
-		driver = webdriver.Firefox()
-		driver.maximize_window()
-		driver.get(baseUrl)
-		driver.implicitly_wait(10)
+    def testLetsKodeIt(self):
+        baseUrl = "https://letskodeit.teachable.com/p/practice"
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(2)
+
+        # Find the state of the text box
+        textBoxElement = driver.find_element(By.ID, 'displayed-text')
+        print("Text is visible? " +)
+
+        # Click the Hide button
+
+        # Find the state of the text box
+
+        # Click the Show button
+
+        # Find the state of the text box
+
+        # Browser CLose
+
+    def testExpedia(self):
+        baseUrl = "https://www.expedia.com"
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(2)
+
+        flightTab = driver.find_element(By.ID, 'tab-flight-tab-hp')
+        flightTab.click()
+        
+        drpdwnElement = driver.find_element(By.ID, 'flight-age-select-1')
 
 
 he = HiddenElements()
-he.test()
+he.testLetsKodeIt()
