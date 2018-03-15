472da537a2cad928dd44dcc92349b04f335d5166
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-15 14:55:00 -0400

    Finish lesson on '.text' property

--- guided_lessons/useful_methods/get_text.py
+++ guided_lessons/useful_methods/get_text.py
@@ -4,12 +4,19 @@ import time
 
 
 class GetText():
-	def test(self):
-		baseUrl = "https://letskodeit.teachable.com/p/practice"
-		driver = webdriver.Firefox()
-		driver.maximize_window()
-		driver.get(baseUrl)
-		driver.implicitly_wait(10)
+
+    def test(self):
+        baseUrl = "https://letskodeit.teachable.com/p/practice"
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(10)
+
+        openTabElement = driver.find_element(By.ID, 'opentab')
+        elText = openTabElement.text
+        print("Text on element is: " + elText)
+        time.sleep(2)
+        driver.quit()
 
 
 gt = GetText()
