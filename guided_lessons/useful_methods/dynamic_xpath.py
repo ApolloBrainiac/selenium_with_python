3c8bfadc30f94daf645e9e47f2c3272038905751
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-15 20:23:03 -0400

    Include instructor code for dynamic xpath lesson

---
+++ guided_lessons/useful_methods/dynamic_xpath.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class DynamicXpath():
+
+    def test(self):
+        baseUrl = "https://letskodeit.teachable.com/"
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        driver.get(baseUrl)
+
+        # Login -> Lecture "How to lick and type on a web element"
+        driver.find_element(By.LINK_TEXT, "Login").click()
+        email = driver.find_element(By.ID, "user_email")
+        email.send_keys("test@email.com")
+        password = driver.find_element(By.ID, "user_password")
+        password.send_keys("abcabc")
+        driver.find_element(By.NAME, "commit").click()
+
+        # Search for courses
+        searchBox = driver.find_element(By.ID, "search-courses")
+        searchBox.send_keys("JavaScript")
+
+        # Select Course
+        _course = "//div[contains(@class, 'course-listing-title') and contains(text(), '{0}')]"
+
+
+dx = DynamicXpath()
+dx.test()
