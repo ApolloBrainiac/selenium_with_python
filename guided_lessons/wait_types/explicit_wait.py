30f3a791d08ed718cd1a9af14180acdfe3ee4ac3
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-22 19:02:28 -0400

    Include note about lesson issues

--- guided_lessons/wait_types/explicit_wait.py
+++ guided_lessons/wait_types/explicit_wait.py
@@ -1,3 +1,13 @@
+"""
+Note: The lesson, as it is now, is unable to be followed.
+Though I have made some changes to the code so it is better
+adapted to the current Expedia website, the input types for
+the departing and returning dates have changed. They no longer
+appear to accept text. I'll follow along with the video for the
+rest of the lesson, but will return to tweek the code after getting
+a better handle on Calendar dropdowns.
+"""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -7,12 +17,14 @@ class ExplicitWait():
 	def test(self):
 		baseUrl = "https://www.expedia.com"
 		driver = webdriver.Firefox()
+		driver.implicitly_wait(5)
 		driver.maximize_window()
 		driver.get(baseUrl)
 		driver.find_element(By.ID, "tab-flight-tab-hp").click()
 		driver.find_element(By.ID, "flight-origin-hp-flight").send_keys("SFO")
 		driver.find_element(By.ID, "flight-destination-hp-flight").send_keys("NYC")
-		driver.find_element(By.ID, "flight-departing-hp-flight").send_keys("12/24/2018")
+		departDate = driver.find_element(By.ID, "flight-departing-hp-flight")
+		departDate.send_keys("12/24/2018")
 		returnDate = driver.find_element(By.ID, "flight-returning-hp-flight")
 		returnDate.clear()
 		returnDate.send_keys("12/26/2018")
