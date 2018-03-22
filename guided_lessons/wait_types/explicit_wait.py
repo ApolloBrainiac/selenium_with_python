4c1d8ab0fa185fdf249827283699fa2240cde5a4
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-22 19:10:22 -0400

    Include the remainder of the code for the explicit wait lesson

--- guided_lessons/wait_types/explicit_wait.py
+++ guided_lessons/wait_types/explicit_wait.py
@@ -11,6 +11,9 @@ a better handle on Calendar dropdowns.
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import *
 
 
 class ExplicitWait():
@@ -30,7 +33,14 @@ class ExplicitWait():
 		returnDate.send_keys("12/26/2018")
 		driver.find_element(By.XPATH,
 		    "//form[@id='gcw-flights-form-hp-flight']//button[@type='submit']").click()
-		driver.find_element(By.ID, "stopFilter_stops-0").click()
+
+		wait = WebDriverWait(driver, 10, poll_frequency=1,
+							ignored_exceptions=[NoSuchElementException,
+												ElementNotVisibleException,
+												ElementNotSelectableException])
+
+		element = wait.until(EC.element_to_be_clickable((By.ID, ""stopFilter_stops-0"")))
+		element.click()
 
 		time.sleep(2)
 		driver.close()
