e4b5aebabf00aea47cb6e6feb1e96163e0f764b2
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-29 17:12:05 -0400

    Begin lesson on generic method for screenshots

---
+++ guided_lessons/advanced_webdriver/screenshots_generic.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+class screenShots():
+    
+    def test(self):
+        baseUrl = "https://letskodeit.teachable.com"
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(5)
+
+        driver.find_element(By.LINK_TEXT, "Login").click()
+        driver.find_element(By.ID, "user_email").send_keys("abc@email.com")
+        driver.find_element(By.ID, "user_password").send_keys("abc")
+        driver.find_element(By.NAME, "commit").click()
+        destinationFileName = "C:\\Users\\ThunderBear\\Pictures\\test.png"
+
+        try:
+            driver.save_screenshot(destinationFileName)
+            print(
+                "Screenshot saved to directory --> :: " + destinationFileName)
+        except NotADirectoryError:
+            print("Not a directory issue")
+
+        time.sleep(3)
+        driver.quit()
+
+    def takeScreenshot(self, driver):
+
+
+
+ss = screenShots()
+ss.test()
