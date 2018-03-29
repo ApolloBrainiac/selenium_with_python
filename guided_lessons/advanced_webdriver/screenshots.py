7aa28601e3c1969c215003ce0647204822473f7e
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-29 17:06:54 -0400

    Update screenshot lesson code

--- guided_lessons/advanced_webdriver/screenshots.py
+++ guided_lessons/advanced_webdriver/screenshots.py
@@ -5,16 +5,23 @@ import time
 
 class screenShots():
     def test(self):
-        baseUrl = "https://letskodit.teachable.com"
+        baseUrl = "https://letskodeit.teachable.com"
         driver = webdriver.Firefox()
         driver.maximize_window()
         driver.get(baseUrl)
         driver.implicitly_wait(5)
 
         driver.find_element(By.LINK_TEXT, "Login").click()
-        driver.find_elment(By.ID, "user_email").send_keys("abc@emial.com")
+        driver.find_element(By.ID, "user_email").send_keys("abc@email.com")
         driver.find_element(By.ID, "user_password").send_keys("abc")
         driver.find_element(By.NAME, "commit").click()
+        destinationFileName = "C:\\Users\\ThunderBear\\Pictures\\test.png"
+
+        try:
+            driver.save_screenshot(destinationFileName)
+            print("Screenshot saved to directory --> :: " + destinationFileName)
+        except NotADirectoryError:
+            print("Not a directory issue")
 
         time.sleep(3)
         driver.quit()
