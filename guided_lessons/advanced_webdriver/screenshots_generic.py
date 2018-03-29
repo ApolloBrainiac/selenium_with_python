7f573b625110c7c5900df4a6c6728733a40264bb
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-29 17:21:25 -0400

    Create 'takeScreenshot' function

--- guided_lessons/advanced_webdriver/screenshots_generic.py
+++ guided_lessons/advanced_webdriver/screenshots_generic.py
@@ -4,7 +4,7 @@ import time
 
 
 class screenShots():
-    
+
     def test(self):
         baseUrl = "https://letskodeit.teachable.com"
         driver = webdriver.Firefox()
@@ -26,10 +26,16 @@ class screenShots():
             print("Not a directory issue")
 
         time.sleep(3)
-        driver.quit()
 
     def takeScreenshot(self, driver):
-
+        """
+        Takes screenshot of the curent open web page
+        :param driver:
+        :return:
+        """
+        fileName = str(round(time.time() * 1000)) + ".png"
+        screenshotDirectory = "C:\\Users\\ThunderBear\\Pictures\\"
+        destinationFile = screenshotDirectory + fileName
 
 
 ss = screenShots()
