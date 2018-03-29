609f045469536f3827594fb3606b73f9b65292b5
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-29 13:54:51 -0400

    Finish date selection path for calendar lesson, test clean

--- guided_lessons/advanced_webdriver/calendar_selection.py
+++ guided_lessons/advanced_webdriver/calendar_selection.py
@@ -13,13 +13,17 @@ class calendarSelect():
         driver.implicitly_wait(5)
 
         # Click flights tab
-        driver.find_element(By.ID, "tab-flight-tab-hp")
+        driver.find_element(By.ID, "tab-flight-tab-hp").click()
         # Find departing field
         departingField = driver.find_element(
             By.ID, "flight-departing-hp-flight")
+        # Click to departing field
+        departingField.click()
         # Find the date to be selected
-        dateToSelect = driver.find_element(By.XPATH, "")
+        dateToSelect = driver.find_element(
+            By.XPATH, "//div[@class='datepicker-cal-month'][position()=1]//button[text()='31']")
         # Click the date
+        dateToSelect.click()
 
         time.sleep(3)
         driver.close()
