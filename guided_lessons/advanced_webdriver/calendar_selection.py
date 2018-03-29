66b59a2c6638b095ba265b0f87966c5f71b8c7d4
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-29 13:26:28 -0400

    Include variables for calendar selection lesson

--- guided_lessons/advanced_webdriver/calendar_selection.py
+++ guided_lessons/advanced_webdriver/calendar_selection.py
@@ -6,11 +6,23 @@ import time
 class calendarSelect():
 
     def test(self):
-        baseUrl = ""
+        baseUrl = "https://www.expedia.com"
         driver = webdriver.Firefox()
         driver.maximize_window()
-        driver.implicitly_wait(5)
         driver.get(baseUrl)
+        driver.implicitly_wait(5)
+
+        # Click flights tab
+        driver.find_element(By.ID, "tab-flight-tab-hp")
+        # Find departing field
+        departingField = driver.find_element(
+            By.ID, "flight-departing-hp-flight")
+        # Find the date to be selected
+        dateToSelect = driver.find_element(By.XPATH, "")
+        # Click the date
+
+        time.sleep(3)
+        driver.close()
 
 
 cs = calendarSelect()
