5ec0085f4d5ddba2be69c0258caec4458ab7c0a3
Andrew M. Perez <AxMPerez@gmail.com>
2018-03-30 17:29:42 -0400

    Finish switch to frame by ID method

--- guided_lessons/windows_iframes/frame_switch.py
+++ guided_lessons/windows_iframes/frame_switch.py
@@ -13,21 +13,25 @@ class FrameSwitch():
         driver.implicitly_wait(5)
         driver.execute_script("window.scrollBy(0, 1000);")
 
-        element = driver.find_element(
-            By.ID, "")
-        element.click()
-        time.sleep(4)
-
         # Switch to frame using Id
+        driver.switch_to.frame("courses-iframe")
 
         # Switch to frame using name
 
         # Switch to frame using numbers
 
+        time.sleep(4)
         # Search course
+        searchBox = driver.find_element(
+            By.ID, "search-courses")
+        searchBox.send_keys("python")
+        time.sleep(4)
 
-        # Switch back to the parent handle
-
+        # Switch back to the parent frame
+        driver.switch_to.default_content()
+        driver.execute_script("window.scrollBy(0, -1000);")
+        time.sleep(4)
+        driver.find_element(By.ID, "name").send_keys("Test Successful")
 
 fs = FrameSwitch()
 fs.test()
