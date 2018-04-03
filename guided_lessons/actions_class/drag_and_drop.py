c9412aa43766ff1ea8ea08380d3d27eb4f33d9c4
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-03 16:42:48 -0400

    Inlcude starter code for drag and drop lesson

---
+++ guided_lessons/actions_class/drag_and_drop.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+import time
+
+
+class DragDrop():
+
+    def test(self):
+        baseUrl = "http://jqueryui.com/droppable/"
+        driver = webdriver.Firefox()
+        driver.maximize_window()
+        driver.get(baseUrl)
+        driver.implicitly_wait(4)
+
+        driver.switch_to.frame(0)
+
+        fromElement = driver.find_element(
+            By.ID, "draggable")
+        toElement = driver.find_element(
+            By.ID, "droppable")
+        time.sleep(2)
+
+        try:
+            print("Drag and drop Element Successful")
+            time.sleep(2)
+        except:
+            print("Drag And Drop failed on element")
+
+
+dd = DragDrop()
+dd.test()
