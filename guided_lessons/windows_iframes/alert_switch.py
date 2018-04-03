a03f5917c6eabe1e6a0fe7f4ed95133ce0ac537b
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-03 14:49:10 -0400

    Finish code for working with JS alerts, practical example

--- guided_lessons/windows_iframes/alert_switch.py
+++ guided_lessons/windows_iframes/alert_switch.py
@@ -13,9 +13,27 @@ class AlertSwitch():
         driver.implicitly_wait(5)
 
         element = driver.find_element(
-            By.ID, "")
-        element.click()
+            By.ID, "name")
+        element.send_keys("Andrew")
         time.sleep(4)
+
+        alertBtn = driver.find_element(
+            By.ID, "alertbtn")
+        alertBtn.click()
+        time.sleep(4)
+
+        alert1 = driver.switch_to.alert
+        alert1.accept()
+        time.sleep(3)
+
+        element.send_keys("Andrew")
+        confirmBtn = driver.find_element(
+            By.ID, "confirmbtn")
+        confirmBtn.click()
+        time.sleep(3)
+
+        alert2 = driver.switch_to.alert
+        alert2.dismiss()
 
 
 al = AlertSwitch()
