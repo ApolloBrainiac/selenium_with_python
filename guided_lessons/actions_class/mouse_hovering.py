2cd32da27e3236ca49afae7b574746f3445b2594
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-03 15:23:24 -0400

    Add starter code for mouse hover lesson

--- guided_lessons/actions_class/mouse_hovering.py
+++ guided_lessons/actions_class/mouse_hovering.py
@@ -1,16 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 import time
 
 
 class MouseHover():
 
-    def test(self):
-        baseUrl = ""
+    def test1(self):
+        baseUrl = "https://letskodeit.teachable.com/pages/practice"
         driver = webdriver.Firefox()
         driver.maximize_window
         driver.get(baseUrl)
         driver.implicitly_wait(5)
+
+        driver.execute_script("window.scrollBy(0, 700);")
+        time.sleep(2)
+        itemToClickLocator = ".//div[@class='mouse-hover-content']//a[text()='Top']"
+        try:
+
+        	print("Mouse Hovered on element")
+        	time.sleep(2)
+
+        	print("Item Clicked")
+
+        except:
+        	print("Mouse Hover failed on element")
 
         element = driver.find_element(
             By.ID, "")
