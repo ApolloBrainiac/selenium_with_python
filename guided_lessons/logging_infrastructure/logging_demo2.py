1b65b55f8087347c5cd824807f6ba3529c18bcd5
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-03 21:47:54 -0400

    Set up 2nd logging demo

---
+++ guided_lessons/logging_infrastructure/logging_demo2.py
@@ -0,0 +1,16 @@
+import logging
+
+
+class LoggingDemo2():
+
+    def method1(self):
+
+    def method2(self):
+
+    def method3(self):
+
+
+ld = LoggingDemo2()
+ld.method1()
+ld.method2()
+ld.method3()
