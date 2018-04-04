20bb5f97748fb57bfb16e27d46e4655022398d7c
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-03 20:22:55 -0400

    Set configuration in logging.conf file

--- guided_lessons/logging_infrastructure/config_logger.py
+++ guided_lessons/logging_infrastructure/config_logger.py
@@ -2,12 +2,15 @@
 Logger Demo
 """
 import logging
+import logging.config
+
 
 class LoggerDemoConf():
 
     def testLog(self):
         # create logger
-
+        logging.config.fileConfig('logging.conf')
+        logger = logging.getLogger(LoggerDemoConf.__name__)
 
         # logging messages
         logger.debug('debug message')
