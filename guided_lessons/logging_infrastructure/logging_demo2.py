4b6da7e7a1e5bdcf93717c9d0239f504829e9a5b
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-03 22:11:25 -0400

    Finish custom logging utility

--- guided_lessons/logging_infrastructure/logging_demo2.py
+++ guided_lessons/logging_infrastructure/logging_demo2.py
@@ -8,24 +8,26 @@ class LoggingDemo2():
 
     def method1(self):
         self.log.debug('debug message')
-        self.log.info('ingo message')
+        self.log.info('info message')
         self.log.warn('warn message')
         self.log.error('error message')
         self.log.critical('critical message')
 
     def method2(self):
-        self.log.debug('debug message')
-        self.log.info('ingo message')
-        self.log.warn('warn message')
-        self.log.error('error message')
-        self.log.critical('critical message')
+        m2Log = cl.customLogger(logging.INFO)
+        m2Log.debug('debug message')
+        m2Log.info('info message')
+        m2Log.warn('warn message')
+        m2Log.error('error message')
+        m2Log.critical('critical message')
 
     def method3(self):
-        self.log.debug('debug message')
-        self.log.info('ingo message')
-        self.log.warn('warn message')
-        self.log.error('error message')
-        self.log.critical('critical message')
+        m3Log = cl.customLogger(logging.INFO)
+        m3Log.debug('debug message')
+        m3Log.info('info message')
+        m3Log.warn('warn message')
+        m3Log.error('error message')
+        m3Log.critical('critical message')
 
 
 ld = LoggingDemo2()
