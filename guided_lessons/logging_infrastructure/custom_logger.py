837a7c7faf1d61572b59ee1b158a08b50b70a429
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-03 21:45:58 -0400

    Finish setting up config for custom logging utility

--- guided_lessons/logging_infrastructure/custom_logger.py
+++ guided_lessons/logging_infrastructure/custom_logger.py
@@ -4,6 +4,20 @@ import logging
 
 def customLogger(logLevel):
     # Gets the name of the class / method from where this method is called
-    loggerName = inspect.stacak()[1][3]
+    loggerName = inspect.stack()[1][3]
+    logger = logging.getLogger(loggerName)
 
     # By default, log all messages
+    logger.setLevel(logging.DEBUG)
+
+    fileHandler = logging.FileHandler(
+        "{0}.log".format(loggerName), mode='w')
+    fileHandler.setLevel(logLevel)
+
+    formatter = logging.Formatter(
+        'format=%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%m/%d/%Y %H:%M:%S')
+    fileHandler.setFormatter(formatter)
+    logger.addHanlder(fileHandler)
+
+    return logger
