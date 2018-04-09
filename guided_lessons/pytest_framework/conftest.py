900f7a328c31d4ee2e9de7cb1d93c79fe41a7dfe
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-09 14:57:09 -0400

    Update conftest methods

--- guided_lessons/pytest_framework/conftest.py
+++ guided_lessons/pytest_framework/conftest.py
@@ -3,13 +3,13 @@ import pytest
 
 @pytest.fixture()
 def setUp():
-    print("Running conftest demo method setUp")
+    print("Running method level setUp")
     yield
-    print("Running conftest demo method tearDown")
+    print("Running method level tearDown")
 
 
 @pytest.fixture(scope="module")
 def oneTimeSetUp():
-    print("Running conftest demo one time setUp")
+    print("Running one time setUp")
     yield
-    print("Running conftest demo one time tearDown")
+    print("Running one time tearDown")
