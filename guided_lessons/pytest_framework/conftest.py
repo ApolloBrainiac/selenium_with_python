6fb16677604371cc85395a2aee6f15313a0fddf8
Andrew M. Perez <AxMPerez@gmail.com>
2018-04-09 15:52:54 -0400

    Update conftest to include functions to accept browser and OS options for tests

--- guided_lessons/pytest_framework/conftest.py
+++ guided_lessons/pytest_framework/conftest.py
@@ -13,3 +13,18 @@ def oneTimeSetUp():
     print("Running one time setUp")
     yield
     print("Running one time tearDown")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--broswer")
+    parser.addoption("--osType", help="Type of opertaing system")
+
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+@pytest.fixture(scope="session")
+def osType(request):
+    return request.config.getoption("--osType")
--- guided_lessons/pytest_framework/test_command_line_args.py
+++ guided_lessons/pytest_framework/test_command_line_args.py
@@ -7,3 +7,18 @@ def test_command_line_methodA(oneTimeSetUp, setUp):
 
 def test_command_line_methodB(oneTimeSetUp, setUp):
     print("Running method B")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--broswer")
+    parser.addoption("--osType", help="Type of opertaing system")
+
+
+@pytest.fixture(scope="session")
+def browser(request):
+    return request.config.getoption("--browser")
+
+
+@pytest.fixture(scope="session")
+def osType(request):
+    return request.config.getoption("--osType")
